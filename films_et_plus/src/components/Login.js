da78c6c03e23127c63028725f7d45b7dbd1a3af6
Mae Anave Caña <mae.cana@outlook.com>
2023-01-08 23:50:58 +0800

    Updaed login background image

--- films_et_plus/src/components/Login.js
+++ films_et_plus/src/components/Login.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Login = (props) => {
     return (
         <Container>
-            <Content>Content</Content>
+            <Content><BgImage /></Content>
         </Container>
     );
 };
@@ -11,8 +11,36 @@ const Login = (props) => {
 const Container = styled.section`
     overflow: hidden;
     display: flex;
+    flex-direction: column;
+    text-align: center;
+    height: 100vh;
 `;
 
-const Content = styled.div``;
+const Content = styled.div`
+    margin-bottom: 10vw;
+    padding: 80px 40px;
+    width: 100%;
+    height: 100%;
+    min-height: 100vh;
+    position relative;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const BgImage = styled.div`
+    height: 100%;
+    background-image: url('/images/login-background.jpg');
+    background-position: top;
+    background-size: cover;
+    background-repeat: no-repeat;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    z-index: -1;
+`;
 
 export default Login;
