aed984df97bafc604d4e2eb18a4a21fbfb5c8028
Mae Anave Caña <mae.cana@outlook.com>
2023-01-20 19:57:17 +0800

    WIP: Added menu item

--- films_et_plus/src/components/Header.js
+++ films_et_plus/src/components/Header.js
@@ -6,22 +6,27 @@ const Header = (props) => {
             <Logo>
                 <img src="/images/logo.svg" alt='Film Et Plus'/>
             </Logo>
+
+            <NavMenu>
+               <a href="/"><img src="/images/home-icon.svg" alt="Home" /><span>HOME</span></a>
+            </NavMenu>
         </Nav>
     );
 };
 
 const Nav = styled.nav`
     height: 70px;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    background-color: #090b13;
     display: flex;
     justify-content: space-between;
     align-items: center;
     padding: 0 36px;
     letter-spacing: 16px;
-    position: fixed;
-    top: 0;
-    left: 0;
-    right: 0;
-    background-color: #090b13
+    z-index: 3;
 `;
 
 const Logo = styled.a`
@@ -38,5 +43,72 @@ const Logo = styled.a`
     }
 `;
 
+
+const NavMenu = styled.div`
+    height: 100%;
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: center;
+    justify-content: flex-end;
+    padding: 0;
+    margin: 0;
+    margin-right: auto;
+    margin-left: 24px;
+    position: relative;
+    letter-spacing: 16px;
+
+    a {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 0 12px;
+
+        img {
+            height: 20px;
+            width: 20px;
+            min-width: 20px;
+            z-index: auto;
+        }
+
+        span {
+            color: rgb(249, 249, 249)
+            font-size: 13px;
+            letter-spacing: 1.42px;
+            line-height: 1.08;
+            padding: 2px 0;
+            white-space: nowrap;
+            position: relative;
+            
+            &:before {
+                opacity: 0;
+                background-color: rgb(249, 249, 249);
+                border-radius: 0 0 4px 4px;
+                bottom: -6px;
+                position: absolute;
+                height: 2px;
+                content: '';
+                left: 0;
+                right: 0;
+                transform-origin: left center;
+                transform: scaleX(0);
+                transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.25) 0s;
+                visibility: hidden;
+                width: auto;
+            }
+        }
+
+        &:hover: {
+            span:before {
+                transform: scaleX(1);
+                visibility: visible;
+                opacity: 1 !important;
+            }
+        }
+    }
+
+    @media (max-width: 768px) {
+        display: none;
+    }
+`;
 
 export default Header;
