c8ee4952e0a1079971f2f79a3d9830ffef0a774d
Mae Anave Caña <mae.cana@outlook.com>
2023-01-25 23:58:34 +0800

    Added home to routing

--- films_et_plus/src/App.js
+++ films_et_plus/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import './App.css';
 import Header from './components/Header';
+import Home from './components/Home';
 import Login from './components/Login';
 
 
@@ -12,6 +13,7 @@ function App() {
       <Router>
         <Routes>
           <Route exact path='/' element={<Login />} />
+          <Route exact path='/home' element={<Home />} />
         </Routes>
       </Router>
     </div>
