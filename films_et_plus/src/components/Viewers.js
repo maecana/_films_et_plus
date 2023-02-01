78c4876d15161ff77926b52d70a62144cb82a904
Mae Anave Caña <mae.cana@outlook.com>
2023-02-01 23:39:11 +0800

    Viewers videos added animation - Added videos onto the viewers component - Added animation on hover

--- films_et_plus/src/components/Viewers.js
+++ films_et_plus/src/components/Viewers.js
@@ -7,22 +7,38 @@ const Viewers = (props) => {
         <Container>
             <Wrap>
                 <img src='/images/viewers-disney.png' alt='Disney' />
+                <video autoPlay={true} loop={true} playsInline={true}>
+                    <source src='/videos/disney.mp4' type='video/mp4' />
+                </video>
             </Wrap>
             <Wrap>
-                <img src='/images/viewers-marvel.png' alt='Disney' />
+                <img src='/images/viewers-marvel.png' alt='Marvel' />
+                <video autoPlay={true} loop={true} playsInline={true}>
+                    <source src='/videos/marvel.mp4' type='video/mp4' />
+                </video>
             </Wrap>
             <Wrap>
-                <img src='/images/viewers-national.png' alt='Disney' />
+                <img src='/images/viewers-national.png' alt='National Geographic' />
+                <video autoPlay={true} loop={true} playsInline={true}>
+                    <source src='/videos/national-geographic.mp4' type='video/mp4' />
+                </video>
             </Wrap>
             <Wrap>
-                <img src='/images/viewers-pixar.png' alt='Disney' />
+                <img src='/images/viewers-pixar.png' alt='Pixar' />
+                <video autoPlay={true} loop={true} playsInline={true}>
+                    <source src='/videos/pixar.mp4' type='video/mp4' />
+                </video>
             </Wrap>
             <Wrap>
-                <img src='/images/viewers-starwars.png' alt='Disney' />
+                <img src='/images/viewers-starwars.png' alt='Starwars' />
+                <video autoPlay={true} loop={true} playsInline={true}>
+                    <source src='/videos/star-wars.mp4' type='video/mp4' />
+                </video>
             </Wrap>
         </Container>
     );
 };
+
 
 const Container = styled.div`
     margin-top: 30px;
@@ -36,6 +52,7 @@ const Container = styled.div`
         grid-template-columns: repeat(1, minmax(0, 1fr));
     }
 `;
+
 
 const Wrap = styled.div`
     padding-top: 56.25%;
@@ -56,8 +73,27 @@ const Wrap = styled.div`
         opacity: 1;
         position: absolute;
         transition: opacity 500ms ease-in-out 0s;
-        z-indexL 1;
+        z-index: 1;
         top: 0;
+    }
+
+    video {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0px;
+        opacity: 0;
+        z-index: 0;
+    }
+
+    &:hover {
+        box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px, rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+        transform: scale(1.05);
+        border-color: rgba(249, 249, 249, 0.8);
+        
+        video {
+            opacity: 1;
+        }
     }
 `;
 
