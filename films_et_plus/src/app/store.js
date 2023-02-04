a080839c8159756f3458256f46fe3cee640276d9
Mae Anave Caña <mae.cana@outlook.com>
2023-02-04 23:52:16 +0800

    Added movie reducer to store

--- films_et_plus/src/app/store.js
+++ films_et_plus/src/app/store.js
@@ -3,6 +3,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import { createLogger } from "redux-logger";
 // Local Imports
 import userReducer from '../features/users/userSlice';
+import movieReducer from '../features/movie/movieSlice';
 
 
 const middleware = [ createLogger() ];
@@ -10,8 +11,9 @@ const middleware = [ createLogger() ];
 export const store = configureStore({
     reducer: {
         user: userReducer,
+        movie: movieReducer,
     },
-    middleware
-})
+    middleware,
+});
 
 export default store;
