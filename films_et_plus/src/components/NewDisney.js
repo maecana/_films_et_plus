429bd9507ed9e6b989701a094c96759c71fa07e6
Mae Anave Caña <mae.cana@outlook.com>
2023-02-07 01:31:13 +0800

    Revert "Added UI for meta content"
    This reverts commit 3967220c55a85242f14085cb1edeada874eb8b9f.

--- films_et_plus/src/components/NewDisney.js
+++ films_et_plus/src/components/NewDisney.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { useSelector } from 'react-redux';
 import { selectNewDisney } from '../features/movie/movieSlice';
 
